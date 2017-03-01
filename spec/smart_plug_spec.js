c809cfb362ae97867cfd01c323febf3944a95106
Col Harris <col.w.harris@gmail.com>
2017-03-01 18:40:40 +0800

    Mocked the socket

--- spec/smart_plug_spec.js
+++ spec/smart_plug_spec.js
@@ -1,10 +1,34 @@
 const nock = require('nock');
 const util = require('util');
+const sinon = require('sinon');
+const net = require('net');
 const SmartPlug = require('../smart_plug');
+const EventEmitter = require('events').EventEmitter;
+const TPLinkProtocol = require('../tp_link_protocol');
+
+function MockSocket() {
+  this.connect = function(port, host, callback) {
+    console.log("mock connect");
+  };
+  this.write = function(msg, encoding, callback) {
+    console.log("mock write");
+    callback();
+  };
+}
+
+util.inherits(MockSocket, EventEmitter);
 
 describe('SmartPlug', () => {
 
   describe('#alias', () => {
+
+    var mockSocket = new MockSocket();
+
+    beforeEach(() => {
+        sinon.stub(net, 'Socket', function() {
+          return mockSocket;
+        });
+    });
 
     it('should return the alias of the device', (done) => {
       var device = new SmartPlug("127.0.0.1", 9999);
@@ -12,6 +36,8 @@ describe('SmartPlug', () => {
           expect(alias).toBe("Mock Device");
           done();
       });
+      var response = {"system":{"get_sysinfo":{"alias": "Mock Device"}}};
+      mockSocket.emit('data', TPLinkProtocol.encrypt(JSON.stringify(response)));
     });
 
   });
